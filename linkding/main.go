54dd093f2f74082b39461687f0e87bc30f1be5f8
Karn Wong <karn@karnwong.me>
2025-02-01 23:49:43 +0700

    linkding: add progress bar

--- linkding/main.go
+++ linkding/main.go
@@ -3,20 +3,15 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"io"
 	"net/http"
 	"os"
 
 	_ "github.com/joho/godotenv/autoload"
+	"github.com/schollz/progressbar/v3"
 )
 
-func main() {
-	urls := []string{
-		"https://en.wikipedia.org/wiki/Carl_Hamilton_novels/",
-	}
-
-	for _, url := range urls {
-		jsonData := []byte(fmt.Sprintf(`{
+func addBookmark(url string) {
+	jsonData := []byte(fmt.Sprintf(`{
   "url": "%s",
   "is_archived": false,
   "unread": false,
@@ -26,23 +21,33 @@ func main() {
   ]
 }`, url))
 
-		req, err := http.NewRequest("POST", fmt.Sprintf("%s/api/bookmarks/", os.Getenv("LINKDING_ENDPOINT")), bytes.NewBuffer(jsonData))
-		if err != nil {
-		}
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/api/bookmarks/", os.Getenv("LINKDING_ENDPOINT")), bytes.NewBuffer(jsonData))
+	if err != nil {
+	}
 
-		req.Header.Set("Content-Type", "application/json")
-		req.Header.Set("Authorization", fmt.Sprintf("Token %s", os.Getenv("LINKDING_TOKEN")))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("Token %s", os.Getenv("LINKDING_TOKEN")))
 
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-		}
-		defer resp.Body.Close()
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+	}
+	defer resp.Body.Close()
 
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-		}
-		fmt.Println(string(body))
+	//body, err := io.ReadAll(resp.Body)
+	//if err != nil {
+	//}
+	//fmt.Println(string(body))
+}
 
+func main() {
+	urls := []string{
+		"https://news.ycombinator.com/item?id=42218828",
+	}
+
+	bar := progressbar.Default(int64(len(urls)))
+	for _, url := range urls {
+		addBookmark(url)
+		bar.Add(1)
 	}
 }
