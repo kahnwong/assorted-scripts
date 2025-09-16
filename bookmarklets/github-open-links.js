f9dd31b87983da77933b4258e7e9888e9fb06c86
Karn Wong <karn@karnwong.me>
2025-09-17 01:04:55 +0700

    use lightweight frontend

--- bookmarklets/github-open-links.js
+++ bookmarklets/github-open-links.js
@@ -3,6 +3,10 @@ javascript: (function () {
     const links = document.querySelectorAll(linksSelector);
 
     links.forEach(link => {
-        window.open(link.href, '_blank');
+        let newHref = link.href;
+        if (newHref.startsWith('https://github.com')) {
+            newHref = newHref.replace('https://github.com', 'http://192.168.1.36:3000');
+        }
+        window.open(newHref, '_blank');
     });
 })();
