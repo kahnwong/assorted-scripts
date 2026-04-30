2756f098ac4f8497f6328d6bde97acf1e0538b79
Karn Wong <karn@karnwong.me>
2026-04-30 17:45:01 +0700

    gitlab: move repo deletion to the end

--- gitlab/github-to-gitlab.sh
+++ gitlab/github-to-gitlab.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 IFS=$'\n' REPOS=($(cat repos.txt))
-for REPO in "${REPOS[@]}"
-do
-    git clone git@github.com:"$GITHUB_ORG/$REPO".git
-    cd "$REPO"
+for REPO in "${REPOS[@]}"; do
+  git clone git@github.com:"$GITHUB_ORG/$REPO".git
+  cd "$REPO" || exit 1
 
-    git remote remove origin
-    glab repo create "$GITLAB_ORG/$REPO"
-    git remote add origin git@gitlab.com:"$GITLAB_ORG/$REPO".git
-    git push -u origin master
+  git remote remove origin
+  glab repo create "$GITLAB_ORG/$REPO"
+  git remote add origin git@gitlab.com:"$GITLAB_ORG/$REPO".git
+  git push -u origin master
 
-    read -p "Delete repo? (y/N): " response
-    if [[ "$response" =~ ^[Yy]$ ]]; then
-      gh repo delete "$GITHUB_ORG/$REPO" --yes
-    fi
 done
+
+read -p "Delete repo? (y/N): " response
+if [[ "$response" =~ ^[Yy]$ ]]; then
+  for REPO in "${REPOS[@]}"; do
+    gh repo delete "$GITHUB_ORG/$REPO" --yes
+  done
+fi
