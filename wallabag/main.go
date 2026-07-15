b9fc1a9dcd5763d6e9c69c6f28f6f829ceaa3fdb
Karn Wong <karn@karnwong.me>
2026-07-15 16:38:02 +0700

    wallabag: write failed urls

--- wallabag/main.go
+++ wallabag/main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"sync"
 
@@ -14,13 +19,63 @@ import (
 
 // might need to re-run to make sure no requests are dropped due to sqlite backend
 
-var tag = "stationery" // CHANGE ME
+var tag = "" // CHANGE ME
 
-func addBookmark(url string, wg *sync.WaitGroup, bar *progressbar.ProgressBar) {
+func postEntry(url, title, tags string, starred, archive int) (int, error) {
+	postData := map[string]string{
+		"url":     url,
+		"title":   title,
+		"tags":    tags,
+		"starred": strconv.Itoa(starred),
+		"archive": strconv.Itoa(archive),
+	}
+	postDataJSON, err := json.Marshal(postData)
+	if err != nil {
+		return 0, err
+	}
+
+	req, err := http.NewRequest("POST", wallabago.LibConfig.WallabagURL+"/api/entries.json", strings.NewReader(string(postDataJSON)))
+	if err != nil {
+		return 0, err
+	}
+
+	authString, err := wallabago.GetAuthTokenHeader()
+	if err != nil {
+		return 0, err
+	}
+
+	req.Header.Add("Authorization", authString)
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	_, err = io.Copy(io.Discard, resp.Body)
+	return resp.StatusCode, err
+}
+
+func writeFailedURL(failedFile *os.File, failedMu *sync.Mutex, url string) {
+	failedMu.Lock()
+	defer failedMu.Unlock()
+
+	if _, err := fmt.Fprintln(failedFile, url); err != nil {
+		log.Printf("Failed to write %s to urls-failed.txt: %v", url, err)
+	}
+}
+
+func addBookmark(url string, wg *sync.WaitGroup, bar *progressbar.ProgressBar, failedFile *os.File, failedMu *sync.Mutex) {
 	defer wg.Done()
 
-	if err := wallabago.PostEntry(url, "", tag, 0, 0); err != nil {
+	statusCode, err := postEntry(url, "", tag, 0, 0)
+	if err != nil {
 		log.Printf("Failed to add %s: %v", url, err)
+		writeFailedURL(failedFile, failedMu, url)
+	} else if statusCode != http.StatusOK {
+		log.Printf("Failed to add %s: status code %d", url, statusCode)
+		writeFailedURL(failedFile, failedMu, url)
 	}
 
 	if err := bar.Add(1); err != nil {
@@ -70,10 +125,17 @@ func main() {
 		log.Fatal("No URLs found in urls.txt")
 	}
 
+	failedFile, err := os.Create("urls-failed.txt")
+	if err != nil {
+		log.Fatalf("Failed to create urls-failed.txt: %v", err)
+	}
+	defer failedFile.Close()
+
 	var wg sync.WaitGroup
+	var failedMu sync.Mutex
 	bar := progressbar.Default(int64(len(urls)))
 
-	// Limit concurrent goroutines to 2
+	// Limit concurrent goroutines
 	semaphore := make(chan struct{}, 4)
 
 	for _, url := range urls {
@@ -83,7 +145,7 @@ func main() {
 			defer func() {
 				<-semaphore // Release semaphore
 			}()
-			addBookmark(u, &wg, bar)
+			addBookmark(u, &wg, bar, failedFile, &failedMu)
 		}(url)
 	}
 
